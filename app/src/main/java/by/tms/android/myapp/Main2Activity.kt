a54b9ddc584c0dfc9c6409caa05d98b92f4e2eed
HolyPygovka <r2598509@gmail.com>
2020-09-18 20:07:16 +0300

    Add button to main 2 layout

--- app/src/main/java/by/tms/android/myapp/Main2Activity.kt
+++ app/src/main/java/by/tms/android/myapp/Main2Activity.kt
@@ -11,12 +11,6 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
-
 }
